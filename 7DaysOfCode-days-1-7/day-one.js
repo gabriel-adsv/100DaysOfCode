2de775e31f58abcd0c74c5819a2bc396648068e1
Gabriel <gabriel.dms@gmail.com>
2024-02-20 23:01:37 -0300

    added first day challenge

---
+++ 7DaysOfCode-days-1-7/day-one.js
@@ -0,0 +1,29 @@
+// DESAFIO - DIA 1
+// Reescreva o código abaixo de maneira que ele imprima as informações de maneira correta, que faça sentido e sem erros:
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+// 1º passo - COMPARAR o numeroUm e a stringUm
+if (numeroUm == stringUm) {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes');
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
+}
+
+// 2º passo - COMPARAR o numeroTrinta e a stringTrinta
+if (numeroTrinta === stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo');
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo');
+}
+
+// 3º passo - COMPARAR o numeroDez e a stringDez
+if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
+}
