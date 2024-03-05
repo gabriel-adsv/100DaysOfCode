9a0ad468ab3beefe9baa26bc9fc53bfcd3c5e3f2
Gabriel <gabriel.dms@gmail.com>
2024-03-05 20:30:55 -0300

    updated challenge day-07.js

--- 7DaysOfCode-days-1-7/day-07/js/app.js
+++ 7DaysOfCode-days-1-7/day-07/js/app.js
@@ -12,3 +12,12 @@ let equal = document.getElementById('equal');
 let calculation = document.getElementById('last-calc');
 let result = document.querySelector('span');
 
+let numberOne = document.getElementById('number-1');
+let numberTwo = document.getElementById('number-2');
+let numberThree = document.getElementById('number-3');
+let numberFour = document.getElementById('number-4');
+let numberFive = document.getElementById('number-5');
+let numberSix = document.getElementById('number-6');
+let numberSeven = document.getElementById('number-7');
+let numberEight = document.getElementById('number-8');
+let numberNine = document.getElementById('number-9');
