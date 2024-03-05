bb21b643c540745597ea6235d4ea7851d38d5f1c
Gabriel <gabriel.dms@gmail.com>
2024-03-05 20:25:10 -0300

    updated challenge day-07.js

---
+++ 7DaysOfCode-days-1-7/day-07/js/app.js
@@ -0,0 +1,14 @@
+let buttonCe = document.querySelector('.Ce');
+let buttonC = document.getElementById('button-c');
+let percent = document.getElementById('percent');
+let division = document.getElementById('division');
+let multiplication = document.getElementById('multiplication');
+let minus = document.getElementById('minus');
+let plus = document.getElementById('plus');
+let plusMinus = document.getElementById('plus-minus');
+let comma = document.getElementById('comma');
+let equal = document.getElementById('equal');
+
+let calculation = document.getElementById('last-calc');
+let result = document.querySelector('span');
+
