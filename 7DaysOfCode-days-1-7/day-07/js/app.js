de2abe909e7f894a3ff90b124ba2deb999220dfb
Gabriel <gabriel.dms@gmail.com>
2024-03-06 23:56:29 -0300

    updated challenge day-07

--- 7DaysOfCode-days-1-7/day-07/js/app.js
+++ 7DaysOfCode-days-1-7/day-07/js/app.js
@@ -1,11 +1,41 @@
-let lastCalc = document.getElementById('last-calc');
-let output = document.getElementById('output');
-let cleanDisplay = document.getElementById('Ce');
+// let lastCalc = document.getElementById('last-calc');
 
-lastCalc.innerHTML = "0";
-output.innerHTML = "0";
+// let number = document.querySelectorAll('#number');
+// // let output = document.getElementById('output');
+// // let cleanDisplay = document.getElementById('Ce');
+// // let exclude = document.getElementById('button-c');
 
-cleanDisplay.addEventListener('click', () => {
-  lastCalc.innerHTML = " ";
-  output.innerHTML = " ";
-});
+// lastCalc.innerHTML = "0";
+// output.innerHTML = "0";
+
+// cleanDisplay.addEventListener('click', () => {
+//   lastCalc.innerHTML = "0";
+//   output.innerHTML = "0";
+// });
+
+
+// exclude.addEventListener('click', () => {
+//   output.innerHTML;
+//   number = output.substring(1);
+//   output.innerHTML = number;
+//   lastCalc.innerHTML = output;
+// });
+
+function colocarAlgo(num){
+  const numero = document.getElementById('resultado').innerHTML;
+  document.getElementById('resultado').innerHTML = numero + num;
+};
+
+function limparDisplay() {
+  document.getElementById('resultado').innerHTML = "";
+};
+
+function calcular(){
+  const resultado = document.getElementById('resultado').innerHTML;
+  if(resultado){
+      document.getElementById('last-calc').innerHTML = resultado;
+      document.getElementById('resultado').innerHTML = eval(resultado);
+  } else {
+      document.getElementById('resultado').innerHTML = "Coloque algo";
+  }
+};
