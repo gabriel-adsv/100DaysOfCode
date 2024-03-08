1985a3fa15cb7512dca22323f900c541fb850427
Gabriel <gabriel.dms@gmail.com>
2024-03-07 23:10:19 -0300

    Added exercise 02 on day-13

--- day-13/exercicio-01.js
+++ day-13/exercicio-01.js
@@ -1,5 +1,5 @@
 // 01. Crie uma função que recebe um array de 2 números e coloque eles em ordem crescente. Não use o método sort.
-let arr = [190, 208]
+let arr = [230, 208]
 
 function ordemCrescente(num1, num2) {
   if(num1 > num2) {
@@ -7,10 +7,8 @@ function ordemCrescente(num1, num2) {
     arr[1] = num1;
     console.log(arr[0], arr[1]);
   } else {
-    arr[0] = num1;
-    arr[1] = num2;
-    console.log(arr[0], arr[1]);
-  }
+    console.log(arr);
+  } 
 }
 
 ordemCrescente(arr[0], arr[1]);
---
+++ day-13/exercicio-02.js
@@ -0,0 +1,1 @@
+// 02. Crie uma função que recebe um array de 3 números e coloque eles em ordem crescente. Crie sua própria função para isso.
