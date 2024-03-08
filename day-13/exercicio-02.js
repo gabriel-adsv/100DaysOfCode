f81bb737a3349e8ce06585eb7bab21ffce3bcfdc
Gabriel <gabriel.dms@gmail.com>
2024-03-07 23:21:17 -0300

    Added exercise 02 on day-13

--- day-13/exercicio-02.js
+++ day-13/exercicio-02.js
@@ -1,1 +1,9 @@
 // 02. Crie uma função que recebe um array de 3 números e coloque eles em ordem crescente. Crie sua própria função para isso.
+let arr = [5, 8, 2];
+
+function ordemCrescente() {
+  console.log(arr.sort());
+}
+
+console.log(arr);
+ordemCrescente(arr);
