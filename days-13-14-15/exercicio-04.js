85362df684e5e1d53662278ffc4973fca9ce36ea
Gabriel <gabriel.dms@gmail.com>
2024-03-10 14:54:24 -0300

    challenge day 14 - exercise 04 finished

--- days-13-14-15/exercicio-04.js
+++ days-13-14-15/exercicio-04.js
@@ -1,3 +1,16 @@
 // Refaça o script anterior, mas agora pergunte ao usuário quantos números ele deseja inserir no array.
 // Depois, o usuário vai preenchendo elemento por elemento do vetor.
 // Ao final, exiba o array e o array ao contrário (generalização do script anterior).
+
+let arr = [];
+let vezesDoUsuario = prompt('Quantos números você quer digitar?');
+let contador = 1;
+ 
+for(let i = 0; i < Number(vezesDoUsuario); i++) {
+  let numero = prompt(`Digite o ${contador}º número:`);
+  arr.push(numero);
+  contador++;
+}
+
+alert(`Você digitou os valores na ordem: ${arr.join(separador = [', '])}`);
+alert(`E a ordem inversa é: ${arr.reverse().join(separador = [', '])}`);
