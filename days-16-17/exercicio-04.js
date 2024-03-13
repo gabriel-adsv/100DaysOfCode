13447174f261ca4cafd1f2a0e0c022562d606d25
Gabriel <gabriel.dms@gmail.com>
2024-03-12 23:16:51 -0300

    added exercicio-04.js

---
+++ days-16-17/exercicio-04.js
@@ -0,0 +1,3 @@
+// MÉTODOS DE ARRAYS - JOIN E SLICE
+// Usando o array de frutas do exercício anterior, use o método join para criar uma string que contenha todas as frutas separadas por vírgulas, use o método slive para criar um novo array que contenha apenas a segunda e a terceira fruta.
+
