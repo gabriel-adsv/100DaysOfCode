9f54f6045742f052203d9d3391ad16146026a4c2
Gabriel <gabriel.dms@gmail.com>
2024-03-13 00:59:35 -0300

    finished exercicio-05.js

---
+++ days-16-17/exercicio-05.js
@@ -0,0 +1,11 @@
+// MÉTODOS DE ITERAÇÃO - MAP
+// Crie um array chamado números com cinco números diferentes. Use o método map para criar um novo array onde cada número seja multiplicado por si mesmo.
+
+let numeros = [5, 6, 7, 8, 9, 10];
+
+let quadrados = numeros.map(function(numero) {
+  return numero * numero;
+});
+
+console.log(quadrados); 
+
