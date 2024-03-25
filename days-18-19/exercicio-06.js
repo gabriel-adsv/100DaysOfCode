ba7c5f7f375e9f98b63bf82c6fc90e2ff17bc08b
Gabriel <gabriel.dms@gmail.com>
2024-03-24 22:49:56 -0300

    resolved exercicio-06.js

--- days-18-19/exercicio-06.js
+++ days-18-19/exercicio-06.js
@@ -1,20 +1,19 @@
 // 6 - Escreva uma função para concatenar dois arrays.
-// 7 - Escreva uma função para inverter um array.
-// 8 - Escreva uma função para ordenar um array de números em ordem crescente.
-// 9 - Escreva uma função para ordenar um array de números em ordem decrescente.
-// 10 - Escreva uma função para encontrar o índice de um elemento em um array.
-// 11 - Escreva uma função para contar o número de ocorrências de um elemento em um array.
-// 12 - Escreva uma função para filtrar elementos pares de um array.
-// 13 - Escreva uma função para encontrar a média dos elementos de um array numérico.
-// 14 - Escreva uma função para encontrar a mediana dos elementos de um array numérico.
-// 15 - Escreva uma função para encontrar o elemento mais frequente em um array.
-// 16 - Escreva uma função para calcular o produto dos elementos de um array numérico.
-// 17 - Escreva uma função para verificar se todos os elementos de um array são positivos.
-// 18 - Escreva uma função para verificar se algum elemento de um array é negativo.
-// 19 - Escreva uma função para encontrar o segundo maior número em um array numérico.
-// 20 - Escreva uma função para encontrar o segundo menor número em um array numérico.
-// 21 - Escreva uma função para calcular a diferença entre duas arrays.
-// 22 - Escreva uma função para criar um novo array sem os elementos fornecidos.
-// 23 - Escreva uma função para gerar um array de números inteiros aleatórios dentro de um intervalo específico.
-// 24 - Escreva uma função para embaralhar os elementos de um array.
-// 25 - Escreva uma função para agrupar os elementos de um array com base em um critério específico.
+function concatenarArrays(array1, array2) {
+  //1º passo: Criar um novo array vazio para armazenar os elementos concatenados
+  let concatenacao = [];
+
+  //2º passo: Adicionar os elementos do primeiro array ao array resultado
+  concatenacao = concatenacao.concat(array1);
+
+  //3º passo: Adicionar os elementos do segundo array ao array resultado
+  concatenacao = concatenacao.concat(array2);
+
+  //4º passo: Retornar o array resultado que contém os elementos concatenados
+  return concatenacao;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [6, 7, 8, 9, 10];
+const resultadoConcatenacao = concatenarArrays(array1, array2);
+console.log(resultadoConcatenacao);
