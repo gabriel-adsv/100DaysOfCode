3b83df50fb45ee3d0c93b5c324928758bd93691d
Gabriel <gabriel.dms@gmail.com>
2024-03-24 21:40:12 -0300

    resolved exercicio-03.js

--- days-18-19/exercicio-03.js
+++ days-18-19/exercicio-03.js
@@ -1,23 +1,17 @@
 // 3 - Escreva uma função para encontrar o menor número em um array numérico.
-// 4 - Escreva uma função para remover elementos duplicados de um array.
-// 5 - Escreva uma função para verificar se um elemento existe em um array.
-// 6 - Escreva uma função para concatenar dois arrays.
-// 7 - Escreva uma função para inverter um array.
-// 8 - Escreva uma função para ordenar um array de números em ordem crescente.
-// 9 - Escreva uma função para ordenar um array de números em ordem decrescente.
-// 10 - Escreva uma função para encontrar o índice de um elemento em um array.
-// 11 - Escreva uma função para contar o número de ocorrências de um elemento em um array.
-// 12 - Escreva uma função para filtrar elementos pares de um array.
-// 13 - Escreva uma função para encontrar a média dos elementos de um array numérico.
-// 14 - Escreva uma função para encontrar a mediana dos elementos de um array numérico.
-// 15 - Escreva uma função para encontrar o elemento mais frequente em um array.
-// 16 - Escreva uma função para calcular o produto dos elementos de um array numérico.
-// 17 - Escreva uma função para verificar se todos os elementos de um array são positivos.
-// 18 - Escreva uma função para verificar se algum elemento de um array é negativo.
-// 19 - Escreva uma função para encontrar o segundo maior número em um array numérico.
-// 20 - Escreva uma função para encontrar o segundo menor número em um array numérico.
-// 21 - Escreva uma função para calcular a diferença entre duas arrays.
-// 22 - Escreva uma função para criar um novo array sem os elementos fornecidos.
-// 23 - Escreva uma função para gerar um array de números inteiros aleatórios dentro de um intervalo específico.
-// 24 - Escreva uma função para embaralhar os elementos de um array.
-// 25 - Escreva uma função para agrupar os elementos de um array com base em um critério específico.
+function menorNumero(array) {
+  let menor = array[0];
+
+  for(let i = 0; i < array.length; i++) {
+    if(menor < array[i]) {
+      menor = menor;
+    } else {
+      menor = array[i];
+    }
+  }
+  return menor;
+}
+
+let numeros = [5, 15, 30, 8, 6, 9, 25];
+let resultado = menorNumero(numeros);
+console.log(resultado);
