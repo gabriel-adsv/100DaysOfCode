8de7a382250c58ecfaf9b7d31ad24ff97c2a6d34
Gabriel <gabriel.dms@gmail.com>
2024-03-25 22:42:00 -0300

    resolved exercicio-10.js

--- days-18-19/exercicio-10.js
+++ days-18-19/exercicio-10.js
@@ -1,4 +1,17 @@
 // 10 - Escreva uma função para encontrar o índice de um elemento em um array.
+function verificaIndice(array, elemento) {
+  let indice = array.indexOf(elemento);
+
+  return indice;
+}
+
+
+let numeros = [1, 2, 3, 4, 5];
+let elemento = 4
+
+let indiceDoElemento = verificaIndice(numeros, elemento);
+console.log(indiceDoElemento);
+
 // 11 - Escreva uma função para contar o número de ocorrências de um elemento em um array.
 // 12 - Escreva uma função para filtrar elementos pares de um array.
 // 13 - Escreva uma função para encontrar a média dos elementos de um array numérico.
