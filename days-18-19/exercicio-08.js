9c02a1aef693bff741da9344a999e074007fd750
Gabriel <gabriel.dms@gmail.com>
2024-03-25 22:19:22 -0300

    resolved exercicio-08.js

--- days-18-19/exercicio-08.js
+++ days-18-19/exercicio-08.js
@@ -1,4 +1,18 @@
 // 8 - Escreva uma função para ordenar um array de números em ordem crescente.
+function ordemCrescente(array) {
+  array.sort(function(a, b) {
+    return a - b;
+  });
+
+  return array;
+};
+
+
+let numeros = [4, 6, 7, 1, 3, 9];
+console.log(numeros);
+
+let numerosOrdenados = ordemCrescente(numeros);
+console.log(numerosOrdenados);
 
 // 9 - Escreva uma função para ordenar um array de números em ordem decrescente.
 // 10 - Escreva uma função para encontrar o índice de um elemento em um array.
