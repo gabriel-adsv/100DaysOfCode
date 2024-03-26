181d0d0ae9cadd847d0091297830303ecbce10d5
Gabriel <gabriel.dms@gmail.com>
2024-03-25 21:54:43 -0300

    added exercicio-08.js

--- days-18-19/exercicio-07.js
+++ days-18-19/exercicio-07.js
@@ -8,22 +8,3 @@ console.log(numeros);
 
 let numerosInvertidos = inverterArray(numeros);
 console.log(numerosInvertidos);
-
-// 8 - Escreva uma função para ordenar um array de números em ordem crescente.
-// 9 - Escreva uma função para ordenar um array de números em ordem decrescente.
-// 10 - Escreva uma função para encontrar o índice de um elemento em um array.
-// 11 - Escreva uma função para contar o número de ocorrências de um elemento em um array.
-// 12 - Escreva uma função para filtrar elementos pares de um array.
-// 13 - Escreva uma função para encontrar a média dos elementos de um array numérico.
-// 14 - Escreva uma função para encontrar a mediana dos elementos de um array numérico.
-// 15 - Escreva uma função para encontrar o elemento mais frequente em um array.
-// 16 - Escreva uma função para calcular o produto dos elementos de um array numérico.
-// 17 - Escreva uma função para verificar se todos os elementos de um array são positivos.
-// 18 - Escreva uma função para verificar se algum elemento de um array é negativo.
-// 19 - Escreva uma função para encontrar o segundo maior número em um array numérico.
-// 20 - Escreva uma função para encontrar o segundo menor número em um array numérico.
-// 21 - Escreva uma função para calcular a diferença entre duas arrays.
-// 22 - Escreva uma função para criar um novo array sem os elementos fornecidos.
-// 23 - Escreva uma função para gerar um array de números inteiros aleatórios dentro de um intervalo específico.
-// 24 - Escreva uma função para embaralhar os elementos de um array.
-// 25 - Escreva uma função para agrupar os elementos de um array com base em um critério específico.
