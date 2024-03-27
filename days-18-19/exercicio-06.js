8a1f8b4ac4a4e03031f08d06cfd3e21879f6584c
Gabriel <gabriel.dms@gmail.com>
2024-03-26 22:42:18 -0300

    revisão do exercicio-06.js

--- days-18-19/exercicio-06.js
+++ days-18-19/exercicio-06.js
@@ -1,19 +1,19 @@
 // 6 - Escreva uma função para concatenar dois arrays.
 function concatenarArrays(array1, array2) {
-  //1º passo: Criar um novo array vazio para armazenar os elementos concatenados
-  let concatenacao = [];
+  let arrayUnico = [];
 
-  //2º passo: Adicionar os elementos do primeiro array ao array resultado
-  concatenacao = concatenacao.concat(array1);
+  arrayUnico = arrayUnico.concat(array1);
 
-  //3º passo: Adicionar os elementos do segundo array ao array resultado
-  concatenacao = concatenacao.concat(array2);
+  arrayUnico = arrayUnico.concat(array2);
 
-  //4º passo: Retornar o array resultado que contém os elementos concatenados
-  return concatenacao;
-}
+  return arrayUnico;
+};
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [6, 7, 8, 9, 10];
-const resultadoConcatenacao = concatenarArrays(array1, array2);
-console.log(resultadoConcatenacao);
+let array1 = [1, 2, 3, 4, 5,];
+console.log(array1);
+
+let array2 = [6, 7, 8, 9, 10];
+console.log(array2);
+
+let concatenacao = concatenarArrays(array1, array2);
+console.log(concatenacao);
