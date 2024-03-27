cb651c36d070acde9d428b12142975910c123779
Gabriel <gabriel.dms@gmail.com>
2024-03-26 22:08:35 -0300

    revisão do exercicio-04.js

--- days-18-19/exercicio-04.js
+++ days-18-19/exercicio-04.js
@@ -5,18 +5,17 @@ function removeDuplicados(array) {
 
   // Passo 2: Iterar sobre cada elemento do array fornecido
   array.forEach(function(elemento) {
-    // Passo 3: Verificar se o elemento atual não está presente no array de elementos únicos
-    if (!unicos.includes(elemento)) {
-      // Passo 4: Se não estiver presente, adicioná-lo ao array de elementos únicos
+    if(!unicos.includes(elemento)) {
       unicos.push(elemento);
     }
   });
-
   // Passo 5: Retornar o array de elementos únicos
   return unicos;
 }
 
 // Exemplo de uso:
-const arrayDuplicados = [1, 2, 2, 3, 4, 4, 5];
+const arrayDuplicados = [1, 2, 2, 3, 3, 4, 4, 5];
+console.log(arrayDuplicados);
+
 const arrayUnicos = removeDuplicados(arrayDuplicados);
 console.log(arrayUnicos); // Saída: [1, 2, 3, 4, 5]
