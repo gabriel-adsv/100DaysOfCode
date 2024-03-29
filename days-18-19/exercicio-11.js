d4d3a219e30d141d600677ec6c94b5e0bc77674c
Gabriel <gabriel.dms@gmail.com>
2024-03-29 16:58:28 -0300

    resolved exercicio-11.js

--- days-18-19/exercicio-11.js
+++ days-18-19/exercicio-11.js
@@ -1,4 +1,18 @@
 // 11 - Escreva uma função para contar o número de ocorrências de um elemento em um array.
+function contadorOcorrencias(array, elemento) {
+  let contador = 0;
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === elemento) {
+      contador++;
+    };
+  };
+  return contador;
+}
+
+let arrayElementos = [1, 2, 3, 4, 2, 2, 3, 2];
+const ocorrencia = 2;
+let numeroDeOcorrencias = contadorOcorrencias(arrayElementos, ocorrencia);
+console.log(`O elemento ${ocorrencia} ocorre ${numeroDeOcorrencias} vezes no array.`);
 
 // 12 - Escreva uma função para filtrar elementos pares de um array.
 // 13 - Escreva uma função para encontrar a média dos elementos de um array numérico.
