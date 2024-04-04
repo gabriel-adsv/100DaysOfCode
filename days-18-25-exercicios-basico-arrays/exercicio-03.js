fe577df0a16a92a98c367e6cc3e5ad4ebf8fd342
Gabriel <gabriel.dms@gmail.com>
2024-04-04 20:38:11 -0300

    Lista de 50 exercicios basicos de arrays: feito 3/50

---
+++ days-18-25-exercicios-basico-arrays/exercicio-03.js
@@ -0,0 +1,6 @@
+// 3 - Acesse o primeiro elemento do array.
+
+let elementosArray = [null, 'String', true, 1];
+
+// Acessando o primeiro elemento do array
+console.log('Primeiro elemento do Array:', elementosArray[0]);
--- exercicio-03.js
+++
@@ -1,4 +0,0 @@
-// 3 - Escreva uma função para encontrar o menor número em um array numérico.
-function menorNumero() {
-
-};
