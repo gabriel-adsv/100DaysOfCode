ecc06c9f1a50149ad32334403bfc803be45865b9
Gabriel <gabriel.dms@gmail.com>
2024-04-04 20:29:41 -0300

    Lista de 50 exercicios basicos de arrays: feito 2/50

--- exercicio-02.js
+++ exercicio-02.js
@@ -1,19 +1,5 @@
-// 2 - Escreva uma função para encontrar o maior número em um array numérico.
+// 2 - Crie um array com 5 números inteiros
 
-function maiorNumero(array) {
-  maiorNumero = array[0];
+let arrayDeInteiros = [1, 2, 3, 4, 5];
 
-  for(let i = 0; i < array.length; i++) {
-    if(maiorNumero < array[i]) {
-      maiorNumero = array[i];
-    };
-  };
-
-  return maiorNumero;
-};
-
-let arrayNumerico = [3, 8, 4, 7, 9, 12, 2, 6, 1, 13];
-
-let resultado = maiorNumero(arrayNumerico);
-
-console.log(resultado);
+console.log('Array com 5 números inteiros:', arrayDeInteiros);
