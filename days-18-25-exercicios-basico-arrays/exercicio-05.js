d7357a691e2b4a41127a75dd72bcf137b08aed9a
Gabriel <gabriel.dms@gmail.com>
2024-04-04 20:55:04 -0300

    Lista de 50 exercicios basicos de arrays: feito 5/50

---
+++ days-18-25-exercicios-basico-arrays/exercicio-05.js
@@ -0,0 +1,13 @@
+// 5 - Adicione um novo elemento ao final do array.
+
+// 1º passo - Criar o array
+let meuArray = [10, 20, 30, 40, 50];
+
+// 2º passo - Exibir o array
+console.log('Array:', meuArray);
+
+// 3º passo - Adicionando um novo elemento no final do array
+meuArray.push(60)
+
+// 4º passo - Exibir array com elemento adicionado ao final
+console.log('Array com novo elemento adicionado no final:', meuArray);
