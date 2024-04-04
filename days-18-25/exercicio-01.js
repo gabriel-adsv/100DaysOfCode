6936c053c7ea4b7a6637371a90852681a4ac9c29
Gabriel <gabriel.dms@gmail.com>
2024-04-04 19:37:00 -0300

    revisão do exercicio 2

--- days-18-25/exercicio-01.js
+++ days-18-25/exercicio-01.js
@@ -1,17 +1,21 @@
 // 1 - Escreva uma função para calcular a soma dos elementos de um array numérico.
-function somarNumeros(array) {
+
+// Criar a função que calcula a soma
+function calculaSoma(array) {
   let soma = 0;
 
   for(let i = 0; i < array.length; i++) {
     soma += array[i];
-  }
+  };
 
   return soma;
-}
+};
 
+// Criar um array numérico
+let arrayNumerico = [1, 2, 3, 4, 5, 6, 7];
 
-let numeros = [1, 2, 3, 4, 5, 6];
-console.log(numeros);
+// Criar variável que vai guardar o resultado da soma
+let resultado = calculaSoma(arrayNumerico);
 
-let resultado = somarNumeros(numeros);
+// Exibir o resultado
 console.log(resultado);
