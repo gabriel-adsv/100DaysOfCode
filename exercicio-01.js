3e4fd679794116b545ae9e9360f0fd7033e626f9
Gabriel <gabriel.dms@gmail.com>
2024-04-04 20:23:31 -0300

    Lista de 50 exercicios basicos de arrays: feito 1/50

---
+++ exercicio-01.js
@@ -0,0 +1,5 @@
+// 1 - Crie um array vazio.
+
+let arrayVazio = [];
+
+console.log('Array vazio:', arrayVazio);
--- exercicio-03.js
+++ exercicio-03.js
@@ -1,21 +1,4 @@
 // 3 - Escreva uma função para encontrar o menor número em um array numérico.
-function menorNumero(array) {
-  let menor = array[0];
+function menorNumero() {
 
-  for(let i = 0; i < array.length; i++) {
-    if(menor > array[i]) {
-      menor = array[i];
-    } else {
-      menor = menor;
-    };
-  };
-
-  return menor;
 };
-
-
-let numeros = [1, 56, 0, 5, 32, 6, 33, 9, 7];
-console.log(numeros);
-
-let resultado = menorNumero(numeros);
-console.log(resultado);
