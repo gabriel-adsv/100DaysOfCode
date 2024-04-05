39da845d5460f2ef14b1afd9413796c8099381ac
Gabriel <gabriel.dms@gmail.com>
2024-04-04 21:12:54 -0300

    Lista de 50 exercicios basicos de arrays: feito 7/50

---
+++ days-18-25-exercicios-basico-arrays/exercicio-07.js
@@ -0,0 +1,13 @@
+// 7 - Adicione um novo elemento ao início do array.
+
+// 1º passo - Criar o array
+let meuArray = [10, 20, 30, 40, 50];
+
+// 2º passo - Exibir o array criado
+console.log('Array:', meuArray);
+
+// 3º passo - Adicionar um elemento no início
+meuArray.unshift(0);
+
+// 4º passo - Exibir o array com o elemento adicionado no início
+console.log('Array com elemento adicionado no início:', meuArray);
