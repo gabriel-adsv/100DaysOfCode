051d1abaddf4c665e9fe9188c177f88f6f51b1e0
Gabriel <gabriel.dms@gmail.com>
2024-04-04 21:30:33 -0300

    Lista de 50 exercicios basicos de arrays: feito 9/50

---
+++ days-18-25-exercicios-basico-arrays/exercicio-09.js
@@ -0,0 +1,10 @@
+// 9 - Verifique o comprimento do array
+
+// 1º passo - Criar o array
+let meuArray = [10, 20, 30, 40, 50, 60, 70];
+
+// 2º passo - exibir o array
+console.log('Array:', meuArray);
+
+// 3º passo - Exibir o tamanho/comprimento do array
+console.log('Comprimento do array:', meuArray.length);
