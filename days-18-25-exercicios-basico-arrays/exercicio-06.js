ce0b19f566c70011aab2b1ac012e7b82bb363aee
Gabriel <gabriel.dms@gmail.com>
2024-04-04 21:03:20 -0300

    Lista de 50 exercicios basicos de arrays: feito 6/50

---
+++ days-18-25-exercicios-basico-arrays/exercicio-06.js
@@ -0,0 +1,13 @@
+// 6 - Remova o último elemento do array.
+
+// 1º passo - Criar o array
+let meuArray = [10, 20, 30, 40, 50];
+
+// 2º passo - Exibir o array criado
+console.log(meuArray);
+
+// 3º passo - Remover o último elemento
+meuArray.pop();
+
+// 4º passo - Exibir o array com o último elemento removido
+console.log('Array com o último elemento removido:', meuArray);
