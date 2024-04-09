a5fb2129e8511b69204ffdb7c0e5ba886a9ae391
Gabriel <gabriel.dms@gmail.com>
2024-04-08 22:50:12 -0300

    Lista de 50 exercicios basicos de arrays: feito 14/50

---
+++ days-18-25-exercicios-basico-arrays/exercicio-14.js
@@ -0,0 +1,12 @@
+// 14 - Multiplique cada elemento do array por 2.
+
+const array = [3, 2, 5, 7, 9];
+
+// forEach()
+// array.forEach((element) => console.log(element * 2));
+
+// for
+for(let i = 0; i < array.length; i++) {
+  let numero = array[i] * 2;
+  console.log(numero);
+}
