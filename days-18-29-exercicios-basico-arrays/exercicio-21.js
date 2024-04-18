77775d15fb067ed41b4daa2f3da8fca4dfab90ec
Gabriel <gabriel.dms@gmail.com>
2024-04-17 23:06:41 -0300

    Lista de 50 exercicios basicos de arrays: criado 22/50

--- days-18-29-exercicios-basico-arrays/exercicio-21.js
+++ days-18-29-exercicios-basico-arrays/exercicio-21.js
@@ -17,10 +17,3 @@ for(const element of segundoArray) {
 
 // 4º passo - Exibir o novo array gerado
 console.log('Novo array gerado:', novoArray);
-
-
-
-// 22 - Gere um novo array com os elementos que estão presentes em pelo menos um dos arrays fornecidos.
-// 23 - Verifique se dois arrays são iguais (ou seja, possuem os mesmos elementos na mesma ordem).
-// 24 - Gere um novo array com os elementos do primeiro array que não estão presentes no segundo array.
-// 25 - Gere um novo array com os elementos que são comuns em todos os arrays fornecidos.
