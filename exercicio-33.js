06c28063236cb04f3f534fde96e3dbf67277a2a7
Gabriel <gabriel.dms@gmail.com>
2024-04-22 22:51:22 -0300

    feito exercicio 33/50

---
+++ exercicio-33.js
@@ -0,0 +1,26 @@
+// 33 - Remova o primeiro elemento do array.
+const array = [0, 1, 2, 3, 4, 5];
+
+console.log('Array antes da remoção do 1º elemento:', array);
+
+array.shift();
+
+console.log('Array após a remoção do 1º elemento:', array);
+
+// Verifique o comprimento do array.
+// Verifique se o array está vazio.
+// Itere sobre todos os elementos do array usando um loop "for".
+// Itere sobre todos os elementos do array usando um loop "for...of".
+// Itere sobre todos os elementos do array usando o método "forEach()".
+// Multiplique cada elemento do array por 2.
+// Encontre o índice de um elemento específico no array.
+// Verifique se um elemento específico está presente no array.
+// Junte todos os elementos do array em uma string.
+// Inverta a ordem dos elementos do array.
+// Ordene os elementos do array em ordem crescente.
+// Ordene os elementos do array em ordem decrescente.
+// Copie os elementos de um array para outro array.
+// Remova o segundo elemento do array.
+// Insira um novo elemento na posição 2 do array.
+// Remova o elemento na posição 3 do array.
+// Substitua o elemento na posição 4 do array por um novo valor.
