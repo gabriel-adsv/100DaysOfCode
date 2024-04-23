6ab5370fc203a2c0f209d94ec4ca831b45174ee0
Gabriel <gabriel.dms@gmail.com>
2024-04-22 22:39:01 -0300

    feito exercicio 30/50

--- lista-exercicios-basico-arrays-pt1/exercicio-30.js
+++ lista-exercicios-basico-arrays-pt1/exercicio-30.js
@@ -1,6 +1,14 @@
 // 30 - Adicione um novo elemento ao final do array.
 const array = [1, 2, 3, 4, 5];
 
+console.log('O array atual é:', array);
+
+array.push(6);
+
+console.log('O elemento adicionado foi o', array[array.length - 1]);
+
+console.log('O array final é:', array);
+
 // Remova o último elemento do array.
 // Adicione um novo elemento ao início do array.
 // Remova o primeiro elemento do array.
