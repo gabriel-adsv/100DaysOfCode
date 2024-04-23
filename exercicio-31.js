06a40ab0ee8086ba729ecc8a19503b4fc0e30317
Gabriel <gabriel.dms@gmail.com>
2024-04-22 22:42:27 -0300

    feito exercicio 31/50

---
+++ exercicio-31.js
@@ -0,0 +1,8 @@
+// 31 - Remova o último elemento do array.
+const array = [1, 2, 3, 4, 5];
+
+console.log('Array antes da remoção do último elemento:', array);
+
+array.pop();
+
+console.log('Array com o último elemento removido:', array);
