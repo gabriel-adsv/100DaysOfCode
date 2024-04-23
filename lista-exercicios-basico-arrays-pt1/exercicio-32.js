a19473444c56161c88a15385539019e9ae7fec01
Gabriel <gabriel.dms@gmail.com>
2024-04-22 22:45:28 -0300

    feito exercicio 32/50

---
+++ lista-exercicios-basico-arrays-pt1/exercicio-32.js
@@ -0,0 +1,8 @@
+// 32 - Adicione um novo elemento ao início do array.
+const array = [1, 2, 3, 4, 5];
+
+console.log('Array atual:', array);
+
+array.unshift(0);
+
+console.log('Array com um elemento adicionado no início', array);
