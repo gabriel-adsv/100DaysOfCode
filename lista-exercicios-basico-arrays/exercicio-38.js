e93ea126c901d447bf72a9d19a0f8a5d64c80c97
Gabriel <gabriel.dms@gmail.com>
2024-04-23 22:14:57 -0300

    feito exercicio 38/50

---
+++ lista-exercicios-basico-arrays/exercicio-38.js
@@ -0,0 +1,6 @@
+// 38 - Itere sobre todos os elementos do array usando o método "forEach()".
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+array.forEach((indice) => {
+  console.log(indice);
+});
