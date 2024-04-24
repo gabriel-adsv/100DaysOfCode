510061f7cc692daee836dc329c5d3e9d759ad265
Gabriel <gabriel.dms@gmail.com>
2024-04-23 22:11:50 -0300

    feito exercicio 37/50

--- lista-exercicios-basico-arrays/exercicio-36.js
+++ lista-exercicios-basico-arrays/exercicio-36.js
@@ -4,19 +4,3 @@ const arrayDeNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for(let i = 0; i < arrayDeNumeros.length; i++) {
   console.log(arrayDeNumeros[i]);
 };
-
-
-// Itere sobre todos os elementos do array usando um loop "for...of".
-// Itere sobre todos os elementos do array usando o método "forEach()".
-// Multiplique cada elemento do array por 2.
-// Encontre o índice de um elemento específico no array.
-// Verifique se um elemento específico está presente no array.
-// Junte todos os elementos do array em uma string.
-// Inverta a ordem dos elementos do array.
-// Ordene os elementos do array em ordem crescente.
-// Ordene os elementos do array em ordem decrescente.
-// Copie os elementos de um array para outro array.
-// Remova o segundo elemento do array.
-// Insira um novo elemento na posição 2 do array.
-// Remova o elemento na posição 3 do array.
-// Substitua o elemento na posição 4 do array por um novo valor.
---
+++ lista-exercicios-basico-arrays/exercicio-37.js
@@ -0,0 +1,6 @@
+// 37 - Itere sobre todos os elementos do array usando um loop "for...of".
+const arrayDeNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8 , 9, 10];
+
+for(let value of arrayDeNumeros) {
+  console.log(value);
+};
