65fa8db5504b82a2ef38e19123d2871dccadc1c0
Gabriel <gabriel.dms@gmail.com>
2024-04-23 22:18:03 -0300

    feito exercicio 39/50

---
+++ lista-exercicios-basico-arrays/exercicio-39.js
@@ -0,0 +1,18 @@
+// 39 - Multiplique cada elemento do array por 2.
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+array.forEach((indice) => {
+  let multiplicacao = indice * 2;
+  console.log(multiplicacao);
+});
+// Encontre o índice de um elemento específico no array.
+// Verifique se um elemento específico está presente no array.
+// Junte todos os elementos do array em uma string.
+// Inverta a ordem dos elementos do array.
+// Ordene os elementos do array em ordem crescente.
+// Ordene os elementos do array em ordem decrescente.
+// Copie os elementos de um array para outro array.
+// Remova o segundo elemento do array.
+// Insira um novo elemento na posição 2 do array.
+// Remova o elemento na posição 3 do array.
+// Substitua o elemento na posição 4 do array por um novo valor.
