103708456e8555c648f8973e5e8fc4690ba052be
Gabriel <gabriel.dms@gmail.com>
2024-04-26 22:01:46 -0300

    atualização no exercicio 39

--- lista-exercicios-basico-arrays/exercicio-39.js
+++ lista-exercicios-basico-arrays/exercicio-39.js
@@ -5,14 +5,3 @@ array.forEach((indice) => {
   let multiplicacao = indice * 2;
   console.log(multiplicacao);
 });
-// Encontre o índice de um elemento específico no array.
-// Verifique se um elemento específico está presente no array.
-// Junte todos os elementos do array em uma string.
-// Inverta a ordem dos elementos do array.
-// Ordene os elementos do array em ordem crescente.
-// Ordene os elementos do array em ordem decrescente.
-// Copie os elementos de um array para outro array.
-// Remova o segundo elemento do array.
-// Insira um novo elemento na posição 2 do array.
-// Remova o elemento na posição 3 do array.
-// Substitua o elemento na posição 4 do array por um novo valor.
